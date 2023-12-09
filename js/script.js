392129ce2ed8a4b191416f14c1cd0feba3c347ae
Nicolas Cugno <nicocugno2017@gmail.com>
2023-12-08 22:15:25 -0300

    Se hace el responsive del index y de dos paginas mas, se agregan funcionalidades, se corrigen errores y se crean las paginas Servicios, Proyectos y Contacto.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+let menuIcon = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
 
 
-
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark')
+    navbar.classList.toggle('active');
+};
 
 
 window.onscroll = () => {
